d68542d7cbdcd3908b8c81434e5753f1c40444f2
codarsssss <codarsssss@gmail.com>
2023-11-03 23:32:33 +0300

    upgrade bootstrap

--- project/home_app/forms.py
+++ project/home_app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class ModelAddForm(forms.Form):
-    one = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control"}))
-    two = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control"}))
-    three = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control"}))
+    one = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control p-1"}))
+    two = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control p-1"}))
+    three = forms.CharField(max_length=255, widget=forms.TextInput(attrs={'class':"form-control p-1"}))
 
