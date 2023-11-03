2998a34b1819b57d1248ba76ac8de000b78d9dc1
codarsssss <codarsssss@gmail.com>
2023-11-03 21:07:46 +0300

    task 1.4

--- project/home_app/views.py
+++ project/home_app/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404
 from django.apps import apps
 from .models import *
@@ -6,24 +5,16 @@ from .models import *
 
 def display_model(request, model_pk=1):
     my_model = get_object_or_404(MyModels, pk=model_pk)
-
-    model = apps.get_model(app_label='home_app', model_name=my_model.name)
-
+    model = apps.get_model('home_app', my_model.name)
     model_data = model.objects.all()
-    print(model_data)
 
-    fields_and_values = []
+    fields = model._meta.get_fields()[1:]
+    fields = fields[model_pk-1:] + fields[:model_pk-1]
 
-    for item in model_data:
-        field_data = {}
-
-        for field in item._meta.fields:
-            field_data[field.name] = getattr(item, field.name)
-
-            fields_and_values.append(field_data)
     return render(request, 'home_app/index.html', {
         'model_data': model_data,
         'model_title': my_model.name,
-        'fields_and_values': fields_and_values,
+        'fields': fields,
+        'pk':model_pk,
+        'max_pk': len(MyModels.objects.all())
     })
-
